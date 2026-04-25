bdb708770f8cc494e01f763847976026af6d93d1
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-04-26 01:33:46 +0300

    fixed linking

--- public/dashboard/dashboard.php
+++ public/dashboard/dashboard.php
@@ -17,7 +17,7 @@ if(!isset($_SESSION["user_id"]))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - IP System</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="disclosure.css">
+    <link rel="stylesheet" href="dashboard.css">
 </head>
 
 <body>
@@ -152,7 +152,7 @@ if(!isset($_SESSION["user_id"]))
         </div>
     </div>
 
-    <script src="js.js"></script>
+    <script src="dashboard.js"></script>
 
 </body>
 
