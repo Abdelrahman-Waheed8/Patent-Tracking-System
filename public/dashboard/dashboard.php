dd3476c0c0e3c867e1c183d1f56795eeccd51ff1
Abdelwhab mohammed <hoba2190@gmail.com>
2026-04-26 12:52:06 +0300

    create disclosure page

--- public/dashboard/dashboard.php
+++ public/dashboard/dashboard.php
@@ -29,10 +29,10 @@ if(!isset($_SESSION["user_id"]))
             <h2 class="logo">IP System</h2>
 
             <ul class="menu">
-                <li><a href="./disclosure.php" class="active"><i class="fas fa-home"></i> Dashboard</a></li>
-                <li><a href="../inventor/inventor.php"><i class="fas fa-user-astronaut"></i>inventore</a></li>
-                <li><a href="#"><i class="fas fa-lightbulb"></i> Disclosure</a></li>
-
+                <li><a href="./dashboard.php" class="active"><i class="fas fa-home"></i> Dashboard</a></li>
+                <li><a href="../inventors/inventors.php"><i class="fas fa-user-astronaut"></i>inventors</a></li>
+                <li><a href="../invention_disclosure/disclosure.php"><i class="fas fa-lightbulb"></i> Disclosure</a>
+                </li>
                 <li><a href="#"><i class="fas fa-file"></i> Patents</a></li>
                 <li><a href="#"><i class="fas fa-sync"></i> Renewals</a></li>
                 <li><a href="#"><i class="fas fa-handshake"></i> Licensing</a></li>
