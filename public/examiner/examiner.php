49961bcce09fcf8fcaf8076488114dac2246793a
Abdelwhab mohammed <hoba2190@gmail.com>
2026-05-02 01:38:40 +0300

    first version of examiner page

---
+++ public/examiner/examiner.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="examiner.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <!-- Navbar -->
+        <nav class="panel-header">
+
+            <div class="left">
+                <i class="fas fa-list icon-btn" onclick="toggleSidebar()"></i>
+                <h2>Examiner Panel</h2>
+            </div>
+
+            <div class="right">
+                <i class="fas fa-bell icon-btn"></i>
+
+                <div class="user icon-btn">
+                    <i class="fas fa-user"></i>
+                    <i class="fas fa-chevron-down small"></i>
+                </div>
+            </div>
+
+        </nav>
+
+        <!-- Content -->
+        <div class="content-wrapper">
+
+            <div class="header-actions">
+                <h2>Pending Patent Reviews</h2>
+            </div>
+            <section>
+                <table id="examiner-table">
+                    <thead>
+                        <tr>
+                            <th>TITLE</th>
+                            <th>INVENTOR</th>
+                            <th>STATUS</th>
+                            <th>ACTION</th>
+                        </tr>
+                        <thead>
+                        <tbody id="table_body">
+
+                        </tbody>
+                </table>
+            </section>
+        </div>
+    </div>
+    <!-- Review Modal -->
+    <div class="modal-overlay" id="review-modal">
+        <div class="modal">
+            <div class="modal-header">
+                <h2>Patent Review</h2>
+                <i class="fas fa-times close-modal"></i>
+            </div>
+            <div id="review-details"></div>
+        </div>
+    </div>
+    <script src="examiner.js"></script>
+</body>
+
+</html>
